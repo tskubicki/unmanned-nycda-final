cc8a3957dc43d674c586a95803a86240d57b6f66
Thomas Skubicki <tskubicki@gmail.com>
2015-08-18 14:49:33 -0400

    added paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	has_attached_file :avatar, :styles =>{
+		:medium => "300x300",
+		:thumb => "100x100",
+		:default_url => "/images/missing.png",
+		:validates_attachment_content_type :avatar,
+		:content_type => /\Aimage\/.*\Z/
+	}
 end
