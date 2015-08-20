8cd2c9a461dae3dce776fef1cfff5418c6c1874c
Thomas Skubicki <tskubicki@gmail.com>
2015-08-20 17:10:17 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'landing/index'
 
   devise_for :users
+  resources :users
+  
   resources :friendships
+  resources :games
+  resources :messages
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
