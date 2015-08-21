7434a092fca751e14929353baefe66d864e9fc77
Thomas Skubicki <tskubicki@gmail.com>
2015-08-21 13:50:28 -0400

    fixed JS issues and added some base stuff to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+	include UsersHelper
+	before_action :check_for_login
+
+	def index
+		@users = User.all
+	end
+
+	def create #post
+		@user = User.new()
+	end
+	
+	def new
+		@user = User.new()
+	end
+	
+	def edit
+		@user = User.new()
+	end
+	
+	def show
+		@user = current_user
+	end
+	
+	def update #patch / put
+		@user = User.find()
+		if @user.update()
+			#success
+		else
+			#fail
+		end
+	end
+	
+	def destroy #delete
+		@user = User.find()
+		if @user.destroy
+			#success
+		else
+			#fail
+		end
+	end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+	def check_for_login
+		unless current_user
+			redirect_to root_path
+		end
+	end
 end
