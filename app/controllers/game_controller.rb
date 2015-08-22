973c6456e167d345c5a4dca6d4d24bd514ed0193
Thomas Skubicki <tskubicki@gmail.com>
2015-08-22 17:10:50 -0400

    set up games posting data into the games table

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,8 +2,30 @@ class GameController < ApplicationController
   def play
   end
   def create
-  	@gamedata = params[:game_data]
-  	redirect_to root_path
+  	@gamedata = JSON.parse(params[:game_data])
+  	@game = Game.new(
+  		user_id: current_user.id,
+  		game_time: @gamedata["game_time"],
+  		score: @gamedata["player_score"],
+  		damage_taken: @gamedata["damage_taken"],
+  		damage_inflicted: @gamedata["damage_inflicted"],
+  		laser_time: @gamedata["laser_time"],
+  		slaser_time: @gamedata["slaser_time"],
+  		mg_time: @gamedata["mg_time"],
+  		rocket_time: @gamedata["rocket_time"],
+  		mine_time: @gamedata["mine_time"],)
+  	if @game.save
+  		#success
+  		redirect_to root_path
+  	else
+  		#fail
+  		redirect_to root_path
+  	end
+  end
+
+  private
+  def game_params
+  	params.require(:game).permit()
   end
 
 end
