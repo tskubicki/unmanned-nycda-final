468bda479c305f56cf92c0c176e0dcce71a195f8
Thomas Skubicki <tskubicki@gmail.com>
2015-08-24 14:41:44 -0400

    added updating of profiles and re-did devise extension

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'landing/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   
   resources :friendships
